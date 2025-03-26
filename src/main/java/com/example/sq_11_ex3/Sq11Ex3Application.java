42b4495c1444653f742307bf6e067d3bd4b3fd9a
TeuschMichael <127761578+TeuschMichael@users.noreply.github.com>
2025-03-26 14:41:01 +0100

    added new project

---
+++ src/main/java/com/example/sq_11_ex3/Sq11Ex3Application.java
@@ -0,0 +1,13 @@
+package com.example.sq_11_ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sq11Ex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sq11Ex3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sq_11_ex3/Sq11Ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sq_11_ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sq11Ex3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
