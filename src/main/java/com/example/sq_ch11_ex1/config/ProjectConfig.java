b5827d31d8bb434d35c748e2b7117836e84c8503
TeuschMichael <127761578+TeuschMichael@users.noreply.github.com>
2025-03-26 13:51:19 +0100

    Spring Start Chapter 11 ex2

--- src/main/java/com/example/sq_ch11_ex1/config/ProjectConfig.java
+++ src/main/java/com/example/sq_ch11_ex1/config/ProjectConfig.java
@@ -1,9 +1,14 @@
 package com.example.sq_ch11_ex1.config;
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients(basePackages = "com.example.sq_ch11_ex1.proxies")
 public class ProjectConfig {
+
+    @Bean
+    public RestTemplate rest(){
+        return new RestTemplate();
+    }
 }
